ab37ec8153c919f16e3a1d868d9fb1e31c459cff
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-21 17:32:59 -0700

    interface with explanations

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+interface A
+{
+    //	public abstract void show();
+    //	public abstract void config();
+
+    /*
+    By default, every method created in an interface is of "public abstract" and there is no need to mention it
+    By default, every variable created in an interface is of "final static", they can't be changed and don't need an object to call them
+     */
+
+    int age=44;            // final and static
+    String area="Mumbai"; // final and static
+
+    void show();    // true form of this is line 3 but no need to mention public abstract in interface
+    void config();  // true form of this is line 4 but no need to mention public abstract in interface
+}
+
+class B implements A
+{
+    public void show()
+    {
+        System.out.println("in show");
+    }
+    public void config()
+    {
+        System.out.println("in cofing");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        A obj;
+        obj=new B();
+
+        obj.show();
+        obj.config();
+
+        //   	A.area="Hyderabad";   this will not work because the variable is in the interface so it is final
+
+        System.out.println(A.area);
+        System.out.println(A.age);
+
     }
 }
